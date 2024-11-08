7cc8ed8f8d219352004c19ca5f63e89b3216fab0
ripples <564311873@qq.com>
2024-11-08 08:08:36 +0000

    feat: optimize build script

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+BUILDER=warp-builder
 GOST_VERSION=2.12.0
 
+if ! grep -q "gost:$GOST_VERSION" Dockerfile; then
+    echo "gost version mismatch"
+    exit 1
+fi
+
 docker pull neilpang/wgcf-docker
-docker build . -t ripples/warp:gost$GOST_VERSION
-docker push ripples/warp:gost$GOST_VERSION
+
+docker buildx create --use --name $BUILDER
+docker buildx inspect --bootstrap
+
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    --output "type=image,push=true" \
+    --tag ripples/warp:gost$GOST_VERSION \
+    --builder $BUILDER .
+
+docker buildx stop $BUILDER
+docker buildx rm $BUILDER
